6130193f84b2afd2a4d01af8feb014ac2e0102e9
Lochlan Bunn <bunn@lochlan.io>
2020-08-28 10:46:57 +0100

    Replace animation action drop with list of actions
    Allows actions to be enabled at the same time, and tracked visually.
    
    Additive version of each action are also available, although due to how this works the underlying clip may be broken...

--- editor/js/Sidebar.Animation.js
+++ editor/js/Sidebar.Animation.js
@@ -1,4 +1,4 @@
-import { UIPanel, UIDiv, UIBreak, UISelect, UIButton, UIText } from './libs/ui.js';
+import { UIPanel, UIRow, UIDiv, UIBreak, UIButton, UINumber, UIText } from './libs/ui.js';
 
 function SidebarAnimation( editor ) {
 
@@ -6,6 +6,7 @@ function SidebarAnimation( editor ) {
 	var mixer = editor.mixer;
 
 	var actions = {};
+	var actionElements = {};
 
 	signals.objectSelected.add( function ( object ) {
 
@@ -17,22 +18,172 @@ function SidebarAnimation( editor ) {
 
 			var options = {};
 			var firstAnimation;
+			var allAnimations = [];
 
 			for ( var animation of animations ) {
+
+				var additiveAnimation = animation.clone();
+				THREE.AnimationUtils.makeClipAdditive( additiveAnimation );
+
+				if ( animation.name.endsWith( '_pose' ) ) {
+
+					animation.name = animation.name + '::clipped';
+
+					animation = THREE.AnimationUtils.subclip(
+						animation,
+						animation.name,
+						1, 30, 30
+					);
+
+				}
+
+				if ( additiveAnimation.name.endsWith( '_pose' ) ) {
+
+					additiveAnimation.name = additiveAnimation.name + '::clipped';
+
+					additiveAnimation = THREE.AnimationUtils.subclip(
+						additiveAnimation,
+						additiveAnimation.name,
+						1, 30, 30
+					);
+
+				}
+
+				additiveAnimation.name = additiveAnimation.name + '::add-blend';
+
+				allAnimations.push( animation );
+				allAnimations.push( additiveAnimation );
+
+			}
+
+			for ( var animation of allAnimations ) {
 
 				if ( firstAnimation === undefined ) firstAnimation = animation.name;
 
 				actions[ animation.name ] = mixer.clipAction( animation, object );
 				options[ animation.name ] = animation.name;
 
-			}
+				actions[ animation.name ].weight = 0;
 
-			animationsSelect.setOptions( options );
-			animationsSelect.setValue( firstAnimation );
+				if ( ! actionElements[ animation.name ] ) {
+
+					var row = new UIRow()
+						.setMarginBottom( '10px' )
+						.setStyle( 'display', [ 'flex' ] )
+						.setStyle( 'align-items', [ 'stretch' ] );
+					container.add( row );
+
+					var leftDiv = new UIDiv()
+						.setStyle( 'display', [ 'flex' ] )
+						.setStyle( 'align-items', [ 'center' ] );
+
+					var playButton = new UIButton( 'Play' )
+						.setMargin( '0 12px 0 4px' )
+						.setWidth( '55px' );
+					playButton.onClick( playAction.bind( null, animation.name ) );
+					leftDiv.add( playButton );
+
+					var rightDiv = new UIDiv();
+
+					rightDiv.add( new UIText( animation.name ) );
+
+					var optionsDiv = new UIDiv().setMarginTop( '4px' );
+
+					var weightDiv = null, weightNum = null;
+
+					if ( animation.blendMode === THREE.AdditiveAnimationBlendMode ) {
+
+						updateActionWeight( animation.name, 0 );
+
+						weightDiv = new UIDiv().setMarginLeft( '4px' );
+						weightDiv.add( new UIText( 'weight' ).setFontWeight( '300' ).setMarginRight( '6px' ) );
+						weightNum = new UINumber( 0 ).setWidth( '50px' );
+						weightNum.min = 0;
+						weightNum.max = 1;
+						weightNum.onChange( updateActionWeight.bind( null, animation.name ) );
+						weightDiv.add( weightNum );
+
+						optionsDiv.add( weightDiv );
+
+					} else {
+
+						row.setMarginBottom( '2px' );
+
+					}
+
+
+					rightDiv.add( optionsDiv );
+
+					row.add( leftDiv );
+					row.add( rightDiv );
+
+					actionElements[ animation.name ] = {};
+					actionElements[ animation.name ].playButton = playButton;
+					actionElements[ animation.name ].weightDiv = weightDiv;
+					actionElements[ animation.name ].weightNum = weightNum;
+
+				}
+
+			}
 
 		} else {
 
 			container.setDisplay( 'none' );
+
+		}
+
+		function getAction( name ) {
+
+			return actions[ name ];
+
+		}
+
+		function playAction( name ) {
+
+			var action = getAction( name );
+
+			if ( action.isRunning() ) {
+
+				updateActionWeight( name, 0 );
+				if ( actionElements[ name ].weightNum ) {
+
+					actionElements[ name ].weightNum.setValue( 0 );
+
+				}
+
+
+				action.stop();
+				actionElements[ name ].playButton.setBackgroundColor( '' );
+				actionElements[ name ].playButton.setTextContent( 'Play' );
+
+			} else {
+
+				action.enabled = true;
+				action.play();
+				actionElements[ name ].playButton.setBackgroundColor( '#165C99' );
+				actionElements[ name ].playButton.setTextContent( 'Stop' );
+
+				updateActionWeight( name, 1 );
+				if ( actionElements[ name ].weightNum ) {
+
+					actionElements[ name ].weightNum.setValue( 1 );
+
+				}
+
+			}
+
+		}
+
+		function updateActionWeight( name, overridingWeight ) {
+
+			var action = getAction( name );
+			var weight = typeof overridingWeight === 'number'
+				? overridingWeight
+				: actionElements[ name ].weightNum.getValue();
+			action.weight = weight;
+			action.enabled = true;
+			action.setEffectiveTimeScale( 1 );
+			action.setEffectiveWeight( weight );
 
 		}
 
@@ -50,32 +201,12 @@ function SidebarAnimation( editor ) {
 
 	} );
 
-	function playAction() {
-
-		actions[ animationsSelect.getValue() ].play();
-
-	}
-
-	function stopAction() {
-
-		actions[ animationsSelect.getValue() ].stop();
-
-	}
-
 	var container = new UIPanel();
 	container.setDisplay( 'none' );
 
 	container.add( new UIText( 'Animations' ).setTextTransform( 'uppercase' ) );
 	container.add( new UIBreak() );
 	container.add( new UIBreak() );
-
-	var div = new UIDiv();
-	container.add( div );
-
-	var animationsSelect = new UISelect().setFontSize( '12px' );
-	div.add( animationsSelect );
-	div.add( new UIButton( 'Play' ).setMarginLeft( '4px' ).onClick( playAction ) );
-	div.add( new UIButton( 'Stop' ).setMarginLeft( '4px' ).onClick( stopAction ) );
 
 	return container;
 
